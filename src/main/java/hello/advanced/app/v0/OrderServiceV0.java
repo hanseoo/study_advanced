355fe96d7c31a460c2d8e9e1c3530ae3d8462dfa
hanseoo <trullo494@gmail.com>
2022-08-02 02:43:23 +0900

    [예제 만들기]로그 추적기 V1 - 프로토타입 개발

---
+++ src/main/java/hello/advanced/app/v0/OrderServiceV0.java
@@ -0,0 +1,15 @@
+package hello.advanced.app.v0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor // 자동으로 final 변수에 대한 생성자 생성
+public class OrderServiceV0 {
+
+    private final OrderRepositoryV0 orderRepository;
+
+    public void orderItem(String itemId) {
+        orderRepository.save(itemId);
+    }
+}
